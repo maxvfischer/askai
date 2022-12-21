5b02bd4869eb5dee9a6b4a283941a66ffd0ff038
Max Fischer <max.fischer@xorlab.com>
2022-12-21 23:53:51 +0100

    Add README to description in setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ setup(
     name="askai",
     version="1.0.0",
     author="Max Fischer",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/maxvfischer/askai",
-
     py_modules=["entrypoint_askai"],
     install_requires=[
         "click==8.1.3",
