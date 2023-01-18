a54dc97159449cee245977be80e5d181b8083e98
Max Fischer <max.fischer@xorlab.com>
2023-01-18 19:55:05 +0100

    Update version

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages, setup  # type: ignore
 
 setup(
     name="askai",
-    version="1.0.3",
+    version="1.0.5",
     author="Max Fischer",
     description="Your simple terminal helper",
     long_description=open("README.md", encoding="utf8").read(),
     long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/maxvfischer/askai",
-    download_url="https://github.com/maxvfischer/askai/archive/refs/tags/v1.0.3.tar.gz",
+    download_url="https://github.com/maxvfischer/askai/archive/refs/tags/v1.0.5.tar.gz",
     packages=["askai"],
     install_requires=[
         "click==8.1.3",
